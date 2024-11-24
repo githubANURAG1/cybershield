5e8ad88e21c55a24e618d0999415a492fede74f7
mallick1126 <rahulmallick1126@gmail.com>
2024-11-24 18:06:02 +0530

    project setup

---
+++ src/test/java/dev/group/cybershield/CybershieldApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.group.cybershield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybershieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
